8a946079d6de58ecba21b046c9418530feb00990
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-21 16:36:43 -0400

    create component and set url api

---
+++ compost-nyc/src/Locations.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class Locations extends Component {
+
+    state ={};
+
+   async componentDidMount(){
+    const url = 'https://data.cityofnewyork.us/resource/if26-z6xq.json?borough=Manhattan';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+        location: data[0].food_scrap_drop_off_site,
+        open: data[0].hours_to,
+        close: data[0].hours_from
+    })
+    
+    console.log(data[0]);
+
+        }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.location} </h1>
+                <h1>Open from : {this.state.open} to {this.state.close}</h1>
+          
+            </div>
+        )
+    }
+}
+
+export default Locations
