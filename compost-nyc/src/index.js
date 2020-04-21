ee5e3e611e97ff6606590f5f182b8c5bb25e1596
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-21 17:49:38 -0400

    wrap App component in BrowserRouter

--- compost-nyc/src/index.js
+++ compost-nyc/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
