886e9b99b361babeb0f161c1302eb9b61a2d04c6
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-22 23:34:42 -0400

    removed div from component

---
+++ compost-nyc/src/components/Borough.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export class Borough extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            site: '',
+            location:'',
+            open: '',
+            close: ''}
+    }
+
+
+   async componentDidMount(){
+    const url = 'https://data.cityofnewyork.us/resource/if26-z6xq.json?borough=Manhattan';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+        site: data[3].food_scrap_drop_off_site,
+        location:data[3].location,
+        open: data[3].hours_to,
+        close: data[3].hours_from
+    })
+    
+    // console.log(data[0]);
+
+        }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.site} - {this.state.location} Open from : {this.state.open} to {this.state.close}</h1>
+          
+            </div>
+        )
+    }
+}
+
+export default Borough;
