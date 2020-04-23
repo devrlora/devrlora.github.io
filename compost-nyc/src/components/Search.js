c581c59f3863a24ec409354c08aaf20c98213490
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-22 21:32:10 -0400

    add form to file

--- compost-nyc/src/components/Search.js
+++ compost-nyc/src/components/Search.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
-export class Search extends Component {
+
+const API_URL = 'https://data.cityofnewyork.us/resource/if26-z6xq.json';
+
+class Search extends Component {
+    state = {
+        query: '',
+        results:[]
+    }
+
     render() {
         return (
             <div>
-                
+                <form >
+                <label>
+                    Borough Name:
+                    <input type = 'text' placeholder= "Search..."
+                     onChange={this.handleInputChange}/>
+                     </label>
+                     <button type='submit'>Search</button>
+                     <p>results go here: {this.state.query}</p>
+                 </form>
             </div>
         )
     }
