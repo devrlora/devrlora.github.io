fb25cbedef3fb929b6aef2e45fc44dcee049e9d6
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-23 00:45:54 -0400

    changed array indice for testing

--- compost-nyc/src/components/Zip.js
+++ compost-nyc/src/components/Zip.js
@@ -4,19 +4,29 @@ export class Zip extends Component {
     constructor(props){
         super(props);
         this.state={
-            site: '',
-            location:'',
-            open: '',
-            close: ''}
+            zip: '',
+           }
     }
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+    async componentDidMount(){
+        const url = 'https://data.cityofnewyork.us/resource/if26-z6xq.json?borough=Manhattan';
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({
+            zip: data[3].zip_code,
+        })
+        
+    
+            }
+        render() {
+            return (
+                <div>
+                    <h1>
+                    {this.state.zip}
+                    </h1>
+                </div>
+            )
+        }
     }
-}
-
-export default Zip
+    
+    export default Zip;
