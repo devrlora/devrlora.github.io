372ca7399c11ed94c9a48eab6f4cbe925d95acc9
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-23 11:23:54 -0400

    changed css path

--- compost-nyc/src/components/Footer.js
+++ compost-nyc/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../components/Footer.css';
+import '../styling/Footer.css';
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
