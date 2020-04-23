c79cfd7ddd993934bbfc4ab2435109dce44c1ac6
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-23 00:34:08 -0400

    add zip component to file

--- compost-nyc/src/components/Zip.js
+++ compost-nyc/src/components/Zip.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export class Zip extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            site: '',
+            location:'',
+            open: '',
+            close: ''}
+    }
+
     render() {
         return (
             <div>
