84d21bb8a2fd9348c4f89cc5f011ff63eec0e05f
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-23 19:14:42 -0400

    add text and formatting

--- compost-nyc/src/components/SignUpText.js
+++ compost-nyc/src/components/SignUpText.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function SignUpText() {
     return (
         <div>
-            <h1>Composting your waste can save up to 21% waste in landfills! If you would like to have your building registered for this service, click the click below to be redirected and apply!</h1>
+            <h1>Composting your waste can save up to 21% waste in landfills! 
+                If you would like to have your building registered for this service, 
+                click the click below to be redirected and apply!</h1>
         </div>
     )
 }
